c4aeceae437cc261daa560e866970aeb812c433d
Anton <antonwelliver27@gmail.com>
2019-11-21 17:10:54 +0100

    added weather api

--- public/script.js
+++ public/script.js
@@ -20,10 +20,26 @@ function getJoke() {
     accessHTTP
         .get("https://api.chucknorris.io/jokes/random")
         .then(theJoke => {
-            console.log(theJoke.value);
             jokeDisplayArea.innerHTML = theJoke.value;
         })
         .catch(err => console.log(err));
 }
 
+function getWeather() {
+    const weatherLocation = document.getElementById("weatherLocation");
+    const weatherDescription = document.getElementById("weatherDescription");
+    const weatherTemperature = document.getElementById("weatherTemperature");
+
+    accessHTTP
+        .get("http://api.openweathermap.org/data/2.5/weather?q=Göteborg,SE&APPID=2244525827f5b74d1d5dbc7bd3cd7474")
+        .then(theWeather => {
+            let temp = parseInt(Number(theWeather.main.temp)) - 273;
+            weatherLocation.innerHTML = theWeather.name;
+            weatherDescription.innerHTML = theWeather.weather[0].description;
+            weatherTemperature.innerHTML = `${temp} °C`;
+        })
+        .catch(err => console.log(err));
+}
+
 getJoke();
+getWeather();
